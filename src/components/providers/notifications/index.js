920d023da81e454ac2a3711115fef5c7fad64e79
Fernando Camargo Del Buono <camargodelbuono@gmail.com>
2020-02-01 01:29:28 +0100

    Customize notifications

--- src/components/providers/notifications/index.js
+++ src/components/providers/notifications/index.js
@@ -1,1 +1,13 @@
-export { SnackbarProvider as default } from 'notistack';
+import { node } from 'prop-types';
+import React from 'react';
+import { SnackbarProvider } from 'notistack';
+
+const Notifications = props => <SnackbarProvider hideIconVariant {...props} />;
+
+Notifications.propTypes = {
+  children: node,
+};
+
+Notifications.defaultProps = {};
+
+export default Notifications;
--- src/hooks/notification/index.js
+++ src/hooks/notification/index.js
@@ -1,12 +1,30 @@
-import { useMemo } from 'react';
+import { useCallback } from 'react';
 import { useSnackbar } from 'notistack';
 
-export default (...params) => {
-  const { enqueueSnackbar: show, closeSnackbar: hide } = useSnackbar();
-  const methods = useMemo(
-    () => ({ notify: show, succeed: show, fail: show, hide }),
-    [show, hide]
+const anchorOrigin = { vertical: 'bottom', horizontal: 'left' };
+
+const defaults = { anchorOrigin };
+
+export default (...settings) => {
+  const { enqueueSnackbar: show, closeSnackbar: hide } = useSnackbar(
+    ...settings
+  );
+  const notify = useCallback(
+    message => show(message, { variant: 'default', ...defaults }),
+    [show]
+  );
+  const warn = useCallback(
+    message => show(message, { variant: 'warning', ...defaults }),
+    [show]
+  );
+  const succeed = useCallback(
+    message => show(message, { variant: 'success', ...defaults }),
+    [show]
+  );
+  const fail = useCallback(
+    message => show(message, { variant: 'error', ...defaults }),
+    [show]
   );
 
-  return methods;
+  return { notify, warn, succeed, fail, hide };
 };
