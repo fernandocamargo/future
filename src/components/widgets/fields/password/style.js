98b36f62825d182e60b9782981dc5782b2056b15
Fernando Camargo Del Buono <camargodelbuono@gmail.com>
2020-02-02 20:13:09 +0100

    Password field improvements

--- src/components/widgets/fields/password/style.js
+++ src/components/widgets/fields/password/style.js
@@ -1,11 +1,20 @@
-import { OutlinedInput } from '@material-ui/core';
-
 import style from 'style';
+import { indigo, red } from '@material-ui/core/colors';
 
 export default style`
-  ${OutlinedInput.type} {
-    .MuiFormControl-root {
-      width: 100%;
+  .MuiFormControl-root {
+    width: 100%;
+  }
+
+  .Mui-error {
+    .MuiIconButton-root {
+      color: ${red[500]};
+    }
+  }
+
+  .Mui-focused:not(.Mui-error) {
+    .MuiIconButton-root {
+      color: ${indigo[500]};
     }
   }
 `;
