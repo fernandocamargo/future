ed7e6988240822ea062bbc50e01b9aef17fbbdea
Fernando Camargo Del Buono <camargodelbuono@gmail.com>
2020-02-17 09:59:05 +0100

    Registration (integration with services)

--- src/hooks/services/expertlead/invitation/index.js
+++ src/hooks/services/expertlead/invitation/index.js
@@ -1,20 +1,28 @@
 import { useCallback } from 'react';
 
+import { useI18n } from 'hooks';
 import { useExpertlead } from 'hooks/clients';
+
+import messages from './messages';
 
 export default token => {
   const expertlead = useExpertlead();
+  const errors = useI18n(messages);
 
   return useCallback(() => {
     const succeed = ({ data: { firstName, lastName, email }, error }) => ({
       name: `${firstName} ${lastName}`,
       email,
     });
-    const fail = ({ message }) => Promise.reject(message);
+    const fail = ({
+      response: {
+        data: { code },
+      },
+    }) => Promise.reject(errors[code]);
 
     return expertlead
       .get(`/invitation/${token}`)
       .then(succeed)
       .catch(fail);
-  }, [expertlead, token]);
+  }, [expertlead, token, errors]);
 };
---
+++ src/hooks/services/expertlead/invitation/messages.js
@@ -0,0 +1,5 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  '5': 'Invalid or expired token',
+});
