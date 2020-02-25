0a63a8566b30fb97e6ab0c88076714e3b203429f
Fernando Camargo Del Buono <f.camargo@expertlead.de>
2020-02-25 18:35:18 +0100

    Testing...

--- src/index.js
+++ src/index.js
@@ -5,6 +5,4 @@ import { Root } from 'components';
 
 const initialize = () => render(<Root />, document.getElementById('root'));
 
-export const foo = 'bar';
-
 export default document.fonts.ready.then(initialize);
