3e9a7e81bc2d8d435389a1eb770ad06de59e559c
Fernando Camargo Del Buono <f.camargo@expertlead.de>
2020-02-25 18:26:08 +0100

    ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,10 @@ const src = path.resolve(__dirname, 'src');
 
 module.exports = {
   env: { browser: true },
+  extends: ['airbnb'],
   settings: {
     'import/resolver': { node: { paths: [src] } },
   },
-  extends: ['airbnb'],
   rules: {
     'react/jsx-filename-extension': 'off',
   },
