1115ec5ba61fa3e4d443acacc7b79ec786d6b705
Fernando Camargo Del Buono <camargodelbuono@gmail.com>
2020-02-26 01:24:05 +0100

    Testing...

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ const path = require('path');
 const src = path.resolve(__dirname, 'src');
 
 module.exports = {
-  env: { browser: true },
+  // env: { browser: true },
   extends: ['react-app', 'airbnb'],
   settings: {
     'import/resolver': { node: { paths: [src] } },
