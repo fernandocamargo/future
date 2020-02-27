e1255f63648d374dafc815ce997c686ca596e26f
Fernando Camargo Del Buono <camargodelbuono@gmail.com>
2020-02-27 01:47:14 +0100

    Add some latency...

--- src/hooks/services/fs/services/index.js
+++ src/hooks/services/fs/services/index.js
@@ -5,5 +5,14 @@ import { useFileSystem } from 'hooks/clients';
 export default () => {
   const { load } = useFileSystem();
 
-  return useCallback(() => load(import('data/services.json')), [load]);
+  return useCallback(
+    () =>
+      load(import('data/services.json')).then(
+        (...params) =>
+          new Promise(resolve =>
+            window.setTimeout(() => resolve(...params), 1000)
+          )
+      ),
+    [load]
+  );
 };
