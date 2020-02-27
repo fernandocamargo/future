7a4919dcf19fa450be7274ef0dd416b4fe03d671
Fernando Camargo Del Buono <camargodelbuono@gmail.com>
2020-02-27 03:23:09 +0100

    Remove fake latency

--- src/hooks/services/fs/services/index.js
+++ src/hooks/services/fs/services/index.js
@@ -5,14 +5,5 @@ import { useFileSystem } from 'hooks/clients';
 export default () => {
   const { load } = useFileSystem();
 
-  return useCallback(
-    () =>
-      load(import('data/services.json')).then(
-        (...params) =>
-          new Promise(resolve =>
-            window.setTimeout(() => resolve(...params), 1000)
-          )
-      ),
-    [load]
-  );
+  return useCallback(() => load(import('data/services.json')), [load]);
 };
